0f4944ce7f15a4ac420593d0f4676434b9161cf4
Finn Fiedler <finn.f@mailbox.org>
2020-01-15 21:27:48 +0100

    created dependency injection skeleton

---
+++ src/farmingDepartment/farmingDepartment.ts
@@ -0,0 +1,6 @@
+/**
+ * Responsible for collecting and storing resources (e.g. energy)
+ */
+export class FarmingDepartment {
+
+}
---
+++ src/xerxesContext.ts
@@ -0,0 +1,6 @@
+/**
+ * The bots global context that stores global information
+ * and also serves als dependency injection container
+ */
+export class XerxesContext {
+}
